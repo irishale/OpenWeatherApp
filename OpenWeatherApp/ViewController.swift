f779a404a93912fc5a3d9f2785eb054391cc5f45
IrishAle <bump.nastey@gmail.com>
2019-09-30 11:08:08 +0400

    Initial Commit

---
+++ OpenWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenWeatherApp
+//
+//  Created by IrishAle on 30/09/2019.
+//  Copyright © 2019 IrishAle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
