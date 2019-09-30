9044627fe6d38d5ff7ee4bc44d7a58236776eabe
IrishAle <bump.nastey@gmail.com>
2019-09-30 22:51:29 +0400

    контроллеры

---
+++ OpenWeatherApp/Presentation/CurrentForecast/CurrentForecastViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CurrentForecastViewController.swift
+//  OpenWeatherApp
+//
+//  Created by IrishAle on 30/09/2019.
+//  Copyright © 2019 IrishAle. All rights reserved.
+//
+
+import UIKit
+
+class CurrentForecastViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
